cb4ae700647ae64a499e3fc4211c109919e4f40c
Aking <www.aking016@gmail.com>
2024-11-24 12:44:15 +0330

    🚀 RELEASE: Demo V0.0.6

--- assets/static/chunks/pages/_error-6ae619510b1539d6.js
+++ assets/static/chunks/pages/_error-6ae619510b1539d6.js
@@ -1,1 +1,1 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{81981:function(n,_,u){(window._assets_P=window._assets_P||[]).push(["/_error",function(){return u(83387)}])}},function(n){n.O(0,[888,774,179],function(){return n(n.s=81981)}),_N_E=n.O()}]);
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([[820], { 81981: function (n, _, u) { (window._assets_P = window._assets_P || []).push(["/_error", function () { return u(83387); }]); } }, function (n) { n.O(0, [888, 774, 179], function () { return n(n.s = 81981); }), _N_E = n.O(); }]);
